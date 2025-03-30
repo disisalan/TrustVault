3387b2138cc6918e2a83fa9d1c6517c249326026
Aayush1805 <aayushvj9876@gmail.com>
2025-03-30 18:32:42 +0530

    frontend routes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginPage from './LoginPage.jsx'
-import Register  from './Register.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <LoginPage /> */}
 
     
-    <Register/>
+    <App/>
   </StrictMode>,
 )
